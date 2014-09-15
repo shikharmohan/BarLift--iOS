8dd741d5aa7fd66cf6e5455a39c11d7607f0e937
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-09-14 20:25:57 -0700

    added night lights background picture to supporting files

--- BarLift/SMDealViewController.m
+++ BarLift/SMDealViewController.m
@@ -27,6 +27,16 @@
 {
     [super viewDidLoad];
     [self.navigationItem setHidesBackButton:YES];
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+        // iOS 6.1 or earlier
+        self.navigationController.navigationBar.tintColor = [UIColor redColor];
+    } else {
+        // iOS 7.0 or later
+        self.navigationController.navigationBar.barTintColor = [UIColor redColor];
+        self.navigationController.navigationBar.translucent = YES;
+    }
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+
     // Do any additional setup after loading the view.
 }
 
